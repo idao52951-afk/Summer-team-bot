fb69287808b2d669a8d2cbb86507949fee44d373
idao52951-afk <idao52951@gmail.com>
2026-09-04 15:46:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import asyncio
 from highrise import BaseBot
 from highrise.__main__ import main
 
-BOT_TOKEN = "5ff2d317c2efd26d3005ee486fb658768d869d075df57774ebabd98a98bac17b"
+    import os
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
 ROOM_ID = "6a95a3ab0cfd91711d40d5d6"
 
 class Bot(BaseBot):
