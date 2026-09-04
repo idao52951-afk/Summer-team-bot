1e4b6d99032f8ea9c98b5bbba49f986a44c4a9eb
idao52951-afk <idao52951@gmail.com>
2026-09-04 15:37:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 from highrise import BaseBot
 from highrise.__main__ import main
 
-BOT_TOKEN = 5ff2d317c2efd26d3005ee486fb658768d869d075df57774ebabd98a98bac17b
-ROOM_ID = https://high.rs/world?id=6894bd39e3e4a405517cb530&ownedRoomId=6a95a3ab0cfd91711d40d5d6&invite_id=6a986f31259f48a4c29ed5cd
+BOT_TOKEN = "5ff2d317c2efd26d3005ee486fb658768d869d075df57774ebabd98a98bac17b"
+ROOM_ID = "6a95a3ab0cfd91711d40d5d6"
 
 class Bot(BaseBot):
     async def on_start(self):
