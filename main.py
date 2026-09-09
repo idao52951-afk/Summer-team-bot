8ea1ba81ead7360d88a7ec9c8d04e1a8bd0b0009
idao52951-afk <idao52951@gmail.com>
2026-09-09 11:14:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "highrise"])
+import os
+import asyncio
+from highrise import BaseBot, Highrise, __main__
+from highrise.models import SessionMetadata, User, AnchorPosition, Reaction, ChatEvent
+
+# On récupère les variables de Railway
+TOKEN = os.getenv("HIGHRISE_TOKEN")
+ROOM_ID = os.getenv("ROOM_ID")
+
+class Bot(BaseBot):
+    async def on_start(self, session_metadata: SessionMetadata):
+        print(f"Bot connecté ! Room: {session_metadata.room_id}")
